05838783354eaaa69381aeb012233ed140a49204
Muhammed Asad Ali Akbar <asad.akberlogo@gmail.com>
2019-11-08 22:45:40 +0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  capturedSnapURL:string;
+ 
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  constructor(private camera: Camera) {}
 
-  constructor() {}
+  takeSnap() {
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.capturedSnapURL = base64Image;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
